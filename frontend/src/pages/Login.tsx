bbb8daaa9f57b0f6d2ee1b732822e010b89dd3b8
Vidarshan <vidarshanadithya3@gmail.com>
2022-01-16 18:33:51 +0530

    Added authentication pages

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div>h</div>;
+};
+
+export default Login;
---
+++ frontend/src/pages/SignUp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUp = () => {
+  return <div>sss</div>;
+};
+
+export default SignUp;
