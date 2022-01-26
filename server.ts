e1d2f861ed72f9605167879b74e58d168f71dce3
Vidarshan <vidarshanadithya3@gmail.com>
2022-01-26 19:02:43 +0530

    Added deployment settigns

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import morgan from "morgan";
+import path from "path";
 
 const app = express();
 
@@ -10,9 +11,18 @@ if (process.env.NODE_ENV === "development") {
 }
 
 const MODE = process.env.NODE_ENV;
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 10100;
 
 dotenv.config();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () =>
   console.log(
