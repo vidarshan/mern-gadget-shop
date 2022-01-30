5922b2b2931b281fefab14bb4113d0a4eb51226e
Vidarshan <vidarshanadithya3@gmail.com>
2022-01-30 19:36:30 +0530

    Added product get routes

--- controllers/productControllers.ts
+++ controllers/productControllers.ts
@@ -11,6 +11,24 @@ const getProducts = asyncHandler(async (req: any, res: any) => {
   res.json({ products });
 });
 
+//@desc         Get product
+//@route        GET /api/products/:id
+//@access       Public
+const getProduct = asyncHandler(async (req: any, res: any) => {
+  const { id } = req.params;
+
+  const product = await Product.findOne({
+    _id: id,
+  });
+
+  if (product) {
+    res.json({ product });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
 //@desc         Create product
 //@route        POST /api/products
 //@access       Public
@@ -45,4 +63,4 @@ const createProduct = asyncHandler(async (req: any, res: any) => {
   res.status(201).json(createdProduct);
 });
 
-export { getProducts, createProduct };
+export { getProducts, getProduct, createProduct };
--- routes/productRoutes.ts
+++ routes/productRoutes.ts
@@ -1,10 +1,14 @@
 import express, { Router } from "express";
 const router = express.Router();
 
-import { createProduct, getProducts } from "../controllers/productControllers";
+import {
+  createProduct,
+  getProduct,
+  getProducts,
+} from "../controllers/productControllers";
 import { protect } from "../middleware/authMiddleware";
 
 router.route("/").get(getProducts).post(protect, createProduct);
-// router.route("/").post(registerUser);
+router.route("/:id").get(getProduct);
 
 export default router;
