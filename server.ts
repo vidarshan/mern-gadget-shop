0af7b897c9be073def06d2b96e8ae199ac3d6f51
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-01 19:57:59 +0530

    Node path fix

--- server.ts
+++ server.ts
@@ -29,11 +29,19 @@ const MODE = process.env.NODE_ENV;
 
 const PORT = process.env.PORT || 10100;
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/build"));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 }
 
