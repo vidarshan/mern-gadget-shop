8441b0d4cae2db61a2201e6b67b7847e3165260c
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-04 21:38:02 +0530

    Changes to cart reducer

--- frontend/src/reducers/cartReducers.ts
+++ frontend/src/reducers/cartReducers.ts
@@ -2,13 +2,15 @@ import {
   ADD_PRODUCT_TO_CART_REQUEST,
   ADD_PRODUCT_TO_CART_SUCCESS,
   ADD_PRODUCT_TO_CART_FAIL,
-  ADD_PRODUCT_TO_CART_RESET,
-  EMPTY_CART_REQUEST,
-  EMPTY_CART_SUCCESS,
-  EMPTY_CART_FAIL,
   GET_CART_ITEMS_REQUEST,
   GET_CART_ITEMS_SUCCESS,
   GET_CART_ITEMS_FAIL,
+  UPDATE_CART_ITEMS_REQUEST,
+  UPDATE_CART_ITEMS_SUCCESS,
+  UPDATE_CART_ITEMS_FAIL,
+  DELETE_ITEMS_REQUEST,
+  DELETE_ITEMS_SUCCESS,
+  DELETE_ITEMS_FAIL,
 } from "../constants/cartConstants";
 
 export const cartReducer = (state = {}, action: any) => {
@@ -34,6 +36,28 @@ export const cartReducer = (state = {}, action: any) => {
         error: null,
       };
     case GET_CART_ITEMS_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    case UPDATE_CART_ITEMS_REQUEST:
+      return { ...state, loading: true, error: null };
+    case UPDATE_CART_ITEMS_SUCCESS:
+      return {
+        ...state,
+        item: action.payload,
+        loading: false,
+        error: null,
+      };
+    case UPDATE_CART_ITEMS_FAIL:
+      return { ...state, loading: false, error: action.payload };
+    case DELETE_ITEMS_REQUEST:
+      return { ...state, loading: true, error: null };
+    case DELETE_ITEMS_SUCCESS:
+      return {
+        ...state,
+        item: action.payload,
+        loading: false,
+        error: null,
+      };
+    case DELETE_ITEMS_FAIL:
       return { ...state, loading: false, error: action.payload };
     default:
       return state;
