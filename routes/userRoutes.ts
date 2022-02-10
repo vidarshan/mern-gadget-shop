41017fc772f9d5fd68f385f6db47c8bc45557c0e
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-10 22:12:34 +0530

    user routes modifications

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -5,9 +5,24 @@ import {
   registerUser,
   authUser,
   getUserProfile,
+  updateUserProfile,
+  deleteUser,
+  getUserById,
+  updateUser,
+  getUsers,
 } from "../controllers/userControllers";
+import { admin, protect } from "../middleware/authMiddleware";
 
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
-router.route("/").post(registerUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
