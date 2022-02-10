0dd029a2924ac16a08cce6ac4aa290315b5b3058
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-10 22:24:33 +0530

    Added routes for products

--- controllers/productControllers.ts
+++ controllers/productControllers.ts
@@ -6,8 +6,24 @@ import generateToken from "../utils/generateToken";
 //@route        GET /api/products
 //@access       Public
 const getProducts = asyncHandler(async (req: any, res: any) => {
-  const products = await Product.find({});
-  res.json({ products });
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 //@desc         Get product
@@ -99,4 +115,77 @@ const createProduct = asyncHandler(async (req: any, res: any) => {
   res.status(201).json(createdProduct);
 });
 
-export { getProducts, getProduct, createProduct, createProductReview };
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req: any, res: any) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(4);
+
+  res.json(products);
+});
+
+// @desc    Fetch single product
+// @route   GET /api/products/:id
+// @access  Public
+const getProductById = asyncHandler(async (req: any, res: any) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Delete a product
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = asyncHandler(async (req: any, res: any) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: "Product removed" });
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req: any, res: any) => {
+  const { name, price, description, image, brand, category, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.brand = brand;
+    product.category = category;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+export {
+  getProducts,
+  getProduct,
+  createProduct,
+  createProductReview,
+  getTopProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+};
