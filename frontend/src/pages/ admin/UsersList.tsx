5a32b2d90aa08c0dc0309c571daa1acc089833ab
Vidarshan <vidarshanadithya3@gmail.com>
2022-02-13 21:05:28 +0530

    users admin section

--- frontend/src/pages/ admin/UsersList.tsx
+++ frontend/src/pages/ admin/UsersList.tsx
@@ -1,9 +1,21 @@
-import { Button, Card, Group, Table, Text } from "@mantine/core";
+import { Button, Card, Group, Pagination, Table, Text } from "@mantine/core";
 import React from "react";
 import Head from "../../components/Head";
 import Layout from "../../layout/Layout";
+import { useDispatch, useSelector } from "react-redux";
+import { bindActionCreators } from "redux";
+import { actionCreators, State } from "../../state";
+import { useEffect, useState } from "react";
+import Loading from "../../components/Loading";
+import moment from "moment";
+import { BiDetail, BiTrashAlt } from "react-icons/bi";
 
 const UsersList = () => {
+  const dispatch = useDispatch();
+  const { getUsers } = bindActionCreators(actionCreators, dispatch);
+
+  const { users, error, loading } = useSelector((state: State) => state.users);
+
   const elements = [
     { position: 6, mass: 12.011, symbol: "C", name: "Carbon" },
     { position: 7, mass: 14.007, symbol: "N", name: "Nitrogen" },
@@ -12,42 +24,63 @@ const UsersList = () => {
     { position: 58, mass: 140.12, symbol: "Ce", name: "Cerium" },
   ];
 
-  const rows = elements.map((element) => (
-    <tr key={element.name}>
-      <td>{element.position}</td>
-      <td>{element.name}</td>
-      <td>{element.symbol}</td>
-      <td>{element.mass}</td>
-    </tr>
-  ));
+  useEffect(() => {
+    getUsers();
+  }, [dispatch]);
+
+  const rows =
+    users &&
+    Object.keys(users).length >= 3 &&
+    users.map((user: any) => (
+      <tr key={user._id}>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.isAdmin ? "Yes" : "No"}</td>
+        <td>{moment(user.createdAt).format("DD-MMM-YYYY hh:mm")}</td>
+
+        <td>
+          <Button color="dark" radius="xl">
+            <BiDetail />
+          </Button>
+        </td>
+        <td>
+          <Button color="red" radius="xl" disabled>
+            <BiTrashAlt />
+          </Button>
+        </td>
+      </tr>
+    ));
 
   return (
     <Layout>
       <Head title="Users List | Admin" />
 
-      <Card withBorder shadow="sm" radius="md" padding="xl">
+      <Card shadow="md" radius="lg">
         <Group sx={{ marginBottom: "1rem" }} direction="row" position="apart">
           <Text weight={700}>Users</Text>
           <Button radius="lg" color="dark">
-            Add new Users
+            Add new User
           </Button>
         </Group>
-        <Table
-          horizontalSpacing="xl"
-          verticalSpacing="xs"
-          striped
-          highlightOnHover
-        >
-          <thead>
-            <tr>
-              <th>Element position</th>
-              <th>Element name</th>
-              <th>Symbol</th>
-              <th>Atomic mass</th>
-            </tr>
-          </thead>
-          <tbody>{rows}</tbody>
-        </Table>
+        {loading ? (
+          <Loading />
+        ) : (
+          <Group position="center" direction="column">
+            <Table horizontalSpacing="xl" verticalSpacing="xs" highlightOnHover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Is Admin</th>
+                  <th>Joined Date</th>
+                  <th></th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>{rows}</tbody>
+            </Table>
+          </Group>
+        )}
       </Card>
     </Layout>
   );
